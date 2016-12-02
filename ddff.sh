700cb77ae3fa65a0dc7dd2c2303c08e959c07487
fabianonunes <fabiano@nunes.me>
2016-12-02 11:23:20 -0200

    desentrelaçar

--- ddff.sh
+++ ddff.sh
@@ -3,6 +3,7 @@ KEY=$1
 
 dd if=/dev/dvb/adapter0/dvr0 conv=noerror | \
 ffmpeg -i - \
+  -deinterlace \
   -c:v libx264 -pix_fmt yuv420p -r 30 -g 60 -preset veryfast \
   -c:a aac -ar 44100 -q:a 3 -b:a 128k \
   -maxrate 1024k -threads 0 -bufsize 1000k \
