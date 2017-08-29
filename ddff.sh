323328a6e8b3c85dcc379bfd7b80809741592362
fabianonunes <fabiano@nunes.me>
2017-08-29 09:40:30 -0300

    lint;

--- ddff.sh
+++ ddff.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 DEST="rtmps://rtmp-api.facebook.com:443/rtmp/"
 KEY=$1
 
@@ -9,4 +11,4 @@ dd if=/dev/dvb/adapter0/dvr0 conv=noerror | \
   -c:v libx264 -pix_fmt yuv420p -r 30 -g 60 -preset veryfast \
   -c:a aac -ar 44100 -q:a 3 -b:a 128k \
   -maxrate 1024k -threads 0 -bufsize 1000k \
-  -f flv $DEST$KEY
+  -f flv "$DEST$KEY"
--- yt2fb.sh
+++ yt2fb.sh
@@ -1,10 +1,12 @@
-SRC=$(youtube-dl -f 93 -g "https://www.youtube.com/watch?v="$2)
+#!/usr/bin/env bash
+
+SRC=$(youtube-dl -f 93 -g "https://www.youtube.com/watch?v=$2")
 DEST="rtmps://rtmp-api.facebook.com:443/rtmp/"
 KEY=$1
 
-ffmpeg -i $SRC \
+ffmpeg -i "$SRC" \
   -deinterlace \
   -c:v libx264 -pix_fmt yuv420p -r 30 -g 60 -preset veryfast \
   -c:a aac -ar 44100 -q:a 3 -b:a 128k \
   -maxrate 1024k -threads 0 -bufsize 1000k \
-  -f flv $DEST$KEY
+  -f flv "$DEST$KEY"
